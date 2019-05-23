18afefb0c073f2f8b97ee909e5d9425041e02287
AlexKatrenich <alex.katrenich@gmail.com>
2019-05-23 22:51:22 +0300

    First commit, RegionsListActivity created

---
+++ RegionsParse/app/src/main/java/com/katrenich/alex/OsmandTestTask/RegionsListActivity.java
@@ -0,0 +1,14 @@
+package com.katrenich.alex.OsmandTestTask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegionsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
